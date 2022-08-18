af87148b85dd10ed9c64ae5db973e92a2873efc7
leoYY <yuan.yi.coding@gmail.com>
2022-08-18 19:48:10 +0800

    update doc

--- docs/.vuepress/config.js
+++ docs/.vuepress/config.js
@@ -4,7 +4,7 @@ const themeConfig = require('./config/themeConfig.js');
 
 module.exports = {
   theme: 'vdoing', // 使用npm包主题
-  title: "yy_blog",
+  title: "YY Blog",
   description: 'vdoing博客主题模板',
   base: '/yy_blog/', // 格式：'/<仓库名>/'， 默认'/'
   markdown: {
