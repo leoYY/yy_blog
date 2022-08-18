914f4ba2cb381094aff631fea6374b282a37132d
leoYY <yuan.yi.coding@gmail.com>
2022-08-18 20:20:42 +0800

    remove docs and test some features

--- docs/.vuepress/config/themeConfig.js
+++ docs/.vuepress/config/themeConfig.js
@@ -4,7 +4,7 @@ const nav = require('./themeConfig/nav.js');
 // 主题配置
 module.exports = {
   nav, // 顶部导航栏
-  sidebarDepth: 2, // 侧边栏显示深度，默认1，最大2（显示到h3标题）
+  sidebarDepth: 1, // 侧边栏显示深度，默认1，最大2（显示到h3标题）
   logo: 'https://github.githubassets.com/images/mona-loading-dark.gif', // 导航栏logo
   repo: 'leoYY/yy_blog', // 导航栏右侧生成Github链接
   searchMaxSuggestions: 10, // 搜索结果显示最大数
@@ -17,9 +17,9 @@ module.exports = {
 
   //*** 以下配置是Vdoing主题改动和新增的配置 ***//
 
-  category: false, // 是否打开分类功能，默认true。 如打开，会做的事情有：1. 自动生成的frontmatter包含分类字段 2.页面中显示与分类相关的信息和模块 3.自动生成分类页面（在@pages文件夹）。如关闭，则反之。
-  tag: false, // 是否打开标签功能，默认true。 如打开，会做的事情有：1. 自动生成的frontmatter包含标签字段 2.页面中显示与标签相关的信息和模块 3.自动生成标签页面（在@pages文件夹）。如关闭，则反之。
-  // archive: false, // 是否打开归档功能，默认true。 如打开，会做的事情有：1.自动生成归档页面（在@pages文件夹）。如关闭，则反之。
+  category: true, // 是否打开分类功能，默认true。 如打开，会做的事情有：1. 自动生成的frontmatter包含分类字段 2.页面中显示与分类相关的信息和模块 3.自动生成分类页面（在@pages文件夹）。如关闭，则反之。
+  tag: true, // 是否打开标签功能，默认true。 如打开，会做的事情有：1. 自动生成的frontmatter包含标签字段 2.页面中显示与标签相关的信息和模块 3.自动生成标签页面（在@pages文件夹）。如关闭，则反之。
+  archive: true, // 是否打开归档功能，默认true。 如打开，会做的事情有：1.自动生成归档页面（在@pages文件夹）。如关闭，则反之。
   // categoryText: '随笔', // 碎片化文章（_posts文件夹的文章）预设生成的分类值，默认'随笔'
 
   // titleBadge: false, // 文章标题前的图标是否显示，默认true
@@ -29,9 +29,9 @@ module.exports = {
   // ],
   // contentBgStyle: 1, // 文章内容块的背景风格，默认无. 1 => 方格 | 2 => 横线 | 3 => 竖线 | 4 => 左斜线 | 5 => 右斜线 | 6 => 点状
 
-  // rightMenuBar: false, // 是否显示右侧文章大纲栏，默认true (屏宽小于1300px下无论如何都不显示)
-  // sidebarOpen: false, // 初始状态是否打开侧边栏，默认true
-  // pageButton: false, // 是否显示快捷翻页按钮，默认true
+  rightMenuBar: true, // 是否显示右侧文章大纲栏，默认true (屏宽小于1300px下无论如何都不显示)
+  sidebarOpen: true, // 初始状态是否打开侧边栏，默认true
+  pageButton: true, // 是否显示快捷翻页按钮，默认true
 
   sidebar: { mode: 'structuring', collapsable: false }, // 侧边栏  'structuring' | { mode: 'structuring', collapsable: Boolean} | 'auto' | 自定义    温馨提示：目录页数据依赖于结构化的侧边栏数据，如果你不设置为'structuring',将无法使用目录页
   pageStyle: 'line', // 页面风格，可选值：'card'卡片 | 'line' 线（未设置bodyBgImg时才生效）， 默认'card'。 说明：card时背景显示灰色衬托出卡片样式，line时背景显示纯色，并且部分模块带线条边框
